b6c62fa179c4321e92d659ed445b95339776e7f2
Anthony Moutte <instabledesign@gmail.com>
2020-03-05 12:32:04 +0100

    move auth and error in middleware option

--- auth/context.go
+++ auth/context.go
@@ -4,9 +4,7 @@ import (
 	"context"
 )
 
-type credentialContextKeyType struct{}
-
-var credentialContextKey = credentialContextKeyType{}
+var credentialContextKey struct{}
 
 func CredentialToContext(ctx context.Context, credential Credential) context.Context {
 	return context.WithValue(ctx, credentialContextKey, credential)
--- middleware/auth.go
+++ middleware/auth.go
@@ -1,22 +1,58 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gol4ng/httpware/v2"
 	"github.com/gol4ng/httpware/v2/auth"
 )
 
-func AuthenticationMiddleware(authFunc auth.AuthFunc) httpware.Middleware {
+func AuthenticationMiddleware(options ...AuthOption) httpware.Middleware {
+	config := NewAuthConfig(options...)
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
-			newCtx, err := authFunc(auth.CredentialToContext(req.Context(), auth.FromHeader(req)()))
-			if err != nil {
-				http.Error(writer, err.Error(), http.StatusUnauthorized)
+			newCtx, err := config.authFunc(req)
+			if err != nil && config.onError(err, writer, req) {
 				return
 			}
 
 			next.ServeHTTP(writer, req.WithContext(newCtx))
 		})
 	}
+}
+
+type AuthFunc func(req *http.Request) (context.Context, error)
+type errorHandler func(err error, writer http.ResponseWriter, req *http.Request) bool
+
+// AuthOption defines a interceptor middleware configuration option
+type AuthOption func(*AuthConfig)
+
+type AuthConfig struct {
+	authFunc AuthFunc
+	onError  errorHandler
+}
+
+func (o *AuthConfig) apply(options ...AuthOption) {
+	for _, option := range options {
+		option(o)
+	}
+}
+
+func NewAuthConfig(options ...AuthOption) *AuthConfig {
+	opts := &AuthConfig{
+		authFunc: DefaultAuthFunc,
+		onError:  DefaultErrorHandler,
+	}
+	opts.apply(options...)
+	return opts
+}
+
+func DefaultAuthFunc(req *http.Request) (context.Context, error) {
+	return auth.CredentialToContext(req.Context(), auth.FromHeader(req)()), nil
+}
+
+func DefaultErrorHandler(err error, writer http.ResponseWriter, _ *http.Request) bool {
+	http.Error(writer, err.Error(), http.StatusUnauthorized)
+	return true
 }
